fa3ad78dbcaaee8ba6073fcf89e90b1dacbbe5fd
Jon Bukiewicz <jonbukiewicz@Jons-Air.attlocal.net>
2023-01-05 22:51:51 -0600

    remove extra style spans

--- src/components/About.js
+++ src/components/About.js
@@ -9,8 +9,8 @@ export default function About(){
 <p><strong>Purpose:&nbsp;</strong><em>This is the day the Lord has made; we will rejoice and be glad in it. Psalm 118:24 NKJV</em></p>
 <p>Commissioning Weekend 2023 will be a time for the territory to come together and <strong>REJOICE</strong> as we celebrate God&rsquo;s faithfulness and witness what He is doing through The Salvation Army in the USA Central Territory.</p>
 <p>Over the three plenary sessions on Friday night, Saturday morning, and Saturday evening we will <strong>REJOICE</strong> over a wide variety of missional expressions through musical and artistic offerings as we highlight our <strong>Mission Imperative: More People More Like Jesus.</strong></p>
-<p><span style="text-align: inherit;">We will also join together in celebrating the faithful service of Commissioners Brad and Heidi Bailey as they enter into retirement.</span></p>
-<p><span style="text-align: inherit;">On Sunday we will witness the sacred Ordination and Commissioning of the Reflectors of Holiness Session as the Cadets are ordained as ministers and commissioned to do the work of The Salvation Army with their new rank of Lieutenant. The weekend will conclude as we all witness the Lieutenants being given their first appointment and sent out to be Officers in His great Army.</span></p>
+<p>We will also join together in celebrating the faithful service of Commissioners Brad and Heidi Bailey as they enter into retirement.</p>
+<p>On Sunday we will witness the sacred Ordination and Commissioning of the Reflectors of Holiness Session as the Cadets are ordained as ministers and commissioned to do the work of The Salvation Army with their new rank of Lieutenant. The weekend will conclude as we all witness the Lieutenants being given their first appointment and sent out to be Officers in His great Army.</p>
     </>
   )
 }
